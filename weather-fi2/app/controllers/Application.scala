97784591480449fd0740263df4f2cee7e9b2dd45
Angelo404 <e.karountzos@hotmail.com>
2014-11-16 15:34:15 +0100

    mongoDB connected

--- weather-fi2/app/controllers/Application.scala
+++ weather-fi2/app/controllers/Application.scala
@@ -2,6 +2,17 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
+import scala.concurrent.Future
+
+// Reactive Mongo imports
+import reactivemongo.api._
+
+// Reactive Mongo plugin, including the JSON-specialized collection
+import play.modules.reactivemongo.MongoController
+import play.modules.reactivemongo.json.collection.JSONCollection
 
 object Application extends Controller {
 
